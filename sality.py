799969db33b0f8eca712283701897513d2a83fe1
Dan611 <dsween34@gmail.com>
2017-11-27 15:08:37 -0600

    Initial commit

--- sality.py
+++ sality.py
@@ -4,13 +4,13 @@ def Dword(i):
 	return ord(file[i]) | (ord(file[i + 1]) << 8) | (ord(file[i + 2]) << 16) | (ord(file[i + 3]) << 24)
 
 # binary specific values
-AMOUNT_TO_DECRYPT = 0x1018 + 0x17E8
 XOR_KEY_OFFSET = 0x77
 ASCII_XOR = 0x66
 
 sality = "Sality_627B8095B1024A0DDFDFA01BF9AFF803.exe"
-ifile = open(sality, 'rb')
+ifile = open(sality, "rb")
 file = ifile.read()
+ifile.close
 
 if Word(0x00) != 0x5A4D:
 	print("Invalid PE file")
@@ -21,27 +21,33 @@ optional_headers = Word(header + 0x14)
 num_sections = Word(header + 0x6)
 section_size = 0x28
 first_section = header + optional_headers + 0x20
-last_section  = header + optional_headers + 0x20 + section_size*(num_sections - 1)
+last_section  = first_section + section_size*(num_sections - 1)
 
 #+0x4 is Virtual Address
+#+0x8 is Raw Size
 #+0xC is Raw Address
-first_rva_offset = -Dword(first_section + 0x4) + Dword(first_section + 0xC)
+first_section_virt = Dword(first_section + 0x4)
+first_section_phys = Dword(first_section + 0xC)
+last_section_size  = Dword(last_section + 0x8)
+last_section_phys  = Dword(last_section + 0xC)
 
-entrypoint = Dword(header + 0x28) + first_rva_offset
-src = Dword(last_section + 0xC)
-xor_key = Dword(entrypoint + XOR_KEY_OFFSET)
+entrypoint = Dword(header + 0x28) - first_section_virt + first_section_phys
+src = last_section_phys						# location to decrypt (last section)
+amount_to_decrypt = last_section_size / 2	# number of words to decrypt
+xor_key = Dword(entrypoint + XOR_KEY_OFFSET)		
 
+# sality begins with PUSHA, CALL+0x55
 if Dword(entrypoint) != 0x0055E860:
 	print("Sality not detected")
 	quit()
 
 # decrypt last section in the binary
 data = bytearray()
-while AMOUNT_TO_DECRYPT > 0:
+while amount_to_decrypt > 0:
 	word = Word(src)
 	src += 2
 
-	xor = (AMOUNT_TO_DECRYPT * xor_key) - (AMOUNT_TO_DECRYPT << 1)
+	xor = (amount_to_decrypt * xor_key) - (amount_to_decrypt << 1)
 	word ^= xor
 
 	# ascii chars encoded via another XOR
@@ -55,15 +61,15 @@ while AMOUNT_TO_DECRYPT > 0:
 	data.append(byte1)
 	data.append(byte2)
 
-	AMOUNT_TO_DECRYPT -= 1
+	amount_to_decrypt -= 1
 
 # get urls from last section
 url = ""
-i = data.find('http')
+i = data.find("http")
 while i != -1:
 	while data[i] != 0:
 		url += chr(data[i])
 		i += 1
 	print(url)
 	url = ""
-	i = data.find('http', i)
+	i = data.find("http", i)
